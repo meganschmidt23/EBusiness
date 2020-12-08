df7e021dfe434765d037631383e6fecf9dca9368
Megan Schmidt <schmidtm6@hawkmail.newpaltz.edu>
2020-12-08 12:38:54 -0500

    Online Catalog Version 1

---
+++ Online Catalog Project/Version 1/config.php
@@ -0,0 +1,15 @@
+<?php
+/* Database credentials. */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'b_f20_36');
+define('DB_PASSWORD', 'ydjysa');
+define('DB_NAME', 'b_f20_36_db');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
